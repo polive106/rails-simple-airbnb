6470d7820ae73ded0cf0fcfe83ef950b710e5550
Pierre Olive <pierre.olive4@gmail.com>
2021-02-10 16:55:27 +0100

    make index logic and UI

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Cleaning database...'
+Flat.destroy_all
+
+10.times do
+  attributes = {
+    address: Faker::Address.full_address,
+    name: Faker::Lorem.sentence(word_count: 5),
+    description: Faker::Lorem.sentence(word_count: 10),
+    price_per_night: rand(10..800),
+    number_of_guests: rand(2..10)
+  }
+  flat = Flat.create!(attributes)
+  puts "Created flat: #{flat.name}"
+end
